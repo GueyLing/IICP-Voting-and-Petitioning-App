cbeb634f10aa457f898e446da4c1da6035ae406f
HneahGueyLing <glhneah@gmail.com>
2022-10-09 01:13:36 +0800

    Petition done

---
+++ app/src/main/java/com/example/adminiicp/CreatePetitionActivity.java
@@ -0,0 +1,14 @@
+package com.example.adminiicp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePetitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_petition);
+    }
+}
