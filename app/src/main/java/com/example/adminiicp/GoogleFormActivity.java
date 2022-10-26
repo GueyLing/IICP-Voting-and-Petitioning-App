e5617976533393ea356f5506882f6fe6e5f5c9bc
HneahGueyLing <glhneah@gmail.com>
2022-10-26 19:53:42 +0800

    feedback and pie chart done

---
+++ app/src/main/java/com/example/adminiicp/GoogleFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.adminiicp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoogleFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_form);
+    }
+}
